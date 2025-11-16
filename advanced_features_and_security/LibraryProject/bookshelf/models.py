c8177e54bc077c6fefdaeaa52e35f01d656459ef
Lebohang Radebe <radebelebohang99@gmail.com>
2025-11-16 15:30:09 +0200

    Managing Permissions and Groups in Django

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,35 +1,24 @@
 from django.db import models
+from django.conf import settings
 
-class Shelf(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    added_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        related_name='books'
+    )
 
     class Meta:
-        verbose_name = 'Shelf'
-        verbose_name_plural = 'Shelves'
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
         permissions = [
-            ("can_view", "Can view shelf"),
-            ("can_create", "Can create shelf"),
-            ("can_edit", "Can edit shelf"),
-            ("can_delete", "Can delete shelf"),
+            ("can_view", "Can view book"),
+            ("can_create", "Can create book"),
+            ("can_edit", "Can edit book"),
+            ("can_delete", "Can delete book"),
         ]
 
     def __str__(self):
-        return self.name
-
-class Section(models.Model):
-    shelf = models.ForeignKey(Shelf, on_delete=models.CASCADE, related_name='sections')
-    name = models.CharField(max_length=100)
-
-    class Meta:
-        verbose_name = 'Section'
-        verbose_name_plural = 'Sections'
-        permissions = [
-            ("can_view", "Can view section"),
-            ("can_create", "Can create section"),
-            ("can_edit", "Can edit section"),
-            ("can_delete", "Can delete section"),
-        ]
-
-    def __str__(self):
-        return f"{self.shelf.name} - {self.name}"
+        return self.title
