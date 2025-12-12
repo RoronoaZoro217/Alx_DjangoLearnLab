54d836961c25b76ca5291b17c66fddf8aa6af32e
Lebohang Radebe <radebelebohang99@gmail.com>
2025-12-12 21:41:34 +0200

    Implement views and serializers in the accounts app for user registration, login, and token retrievals

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
 from django.contrib.auth import authenticate, get_user_model
+from rest_framework.authtoken.models import Token
+
+User = get_user_model()
+
 
 class UserRegistrationSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ['username', 'email', 'password']
 
     def create(self, validated_data):
@@ -14,7 +18,9 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
             email=validated_data.get('email'),
             password=validated_data['password']
         )
+        Token.objects.create(user=user)
         return user
+
 
 class UserLoginSerializer(serializers.Serializer):
     username = serializers.CharField()
