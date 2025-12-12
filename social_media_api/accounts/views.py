acbf87ae942bd038de44963bb90ebe67c54c9a31
Lebohang Radebe <radebelebohang99@gmail.com>
2025-12-12 22:08:54 +0200

    Implementing Posts and Comments Functionality

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from rest_framework import generics, status
+from django.shortcuts import render
+from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from .serializers import UserRegistrationSerializer, UserLoginSerializer
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+# Existing views
 class UserRegistrationView(generics.CreateAPIView):
     serializer_class = UserRegistrationSerializer
 
@@ -18,3 +22,30 @@ class UserLoginView(generics.GenericAPIView):
         user = serializer.validated_data['user']
         token, created = Token.objects.get_or_create(user=user)
         return Response({'token': token.key}, status=status.HTTP_200_OK)
+
+
+# New follow/unfollow views
+class FollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, username, *args, **kwargs):
+        try:
+            target_user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response({"detail": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        request.user.following.add(target_user)
+        return Response({"detail": f"You are now following {username}"}, status=status.HTTP_200_OK)
+
+
+class UnfollowUserView(generics.GenericAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, username, *args, **kwargs):
+        try:
+            target_user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            return Response({"detail": "User not found"}, status=status.HTTP_404_NOT_FOUND)
+
+        request.user.following.remove(target_user)
+        return Response({"detail": f"You have unfollowed {username}"}, status=status.HTTP_200_OK)
